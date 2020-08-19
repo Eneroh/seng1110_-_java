949b8dbe5af17f17a35e03fc495640a0d9b30ee4
eneroh <28052446+Eneroh@users.noreply.github.com>
2020-08-19 16:58:38 +1000

    Create Average3.java

---
+++ Week 1/Average3.java
@@ -0,0 +1,15 @@
+public class Average3
+{
+	
+	public static void main(String[] args)
+	{
+		int num1 = 75;
+		int num2 = 137;
+		int num3 = 265;
+
+		double average = (num1 + num2 + num3) /2;
+
+		System.out.print("The average of " + num1);
+		System.out.println(" and " + num2 + " and " + num3 + " is " + average);
+	}
+}
