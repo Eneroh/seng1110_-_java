5793389bc2546f228cd9cd2184becb9ca465b89c
eneroh <28052446+Eneroh@users.noreply.github.com>
2020-08-23 15:42:47 +1000

    Update helloworld.java

--- labs/week_1/helloworld.java
+++ labs/week_1/helloworld.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!!!");
+	}
+}
+
+import java.util.*;
+
+public class test
+{
+		
+	public static void main (String[] args)
+	{
+		String str = ("Hello World");
+		System.out.println(str);
 	}
 }
